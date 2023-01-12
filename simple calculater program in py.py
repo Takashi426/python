551504cdf974a863006ba01960cda4b861db8252
Takashi426 <101192649+Takashi426@users.noreply.github.com>
2023-01-12 12:29:56 +0530

    Add files via upload

---
+++ simple calculater program in py.py
@@ -0,0 +1,17 @@
+
+print("claculater")
+a=input("enter the frist number")
+a=int(a)
+b=input("entr the secound numbr")
+b=int(b)
+c=input("select + - *")
+if c=='+':
+    print(a+b)
+elif c=='-':
+   print(a-b)
+elif c=='*':
+    print(a*b)
+else:
+    print("enter the correct option")
+
+    
